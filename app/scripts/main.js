9fc9ce35205b7a1e4491bb5c1a668b92e495be33
Filip Muki Dobranic <filipdobranic@gmail.com>
2015-12-30 13:58:47 +0100

    added social icons and "add yours" button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,26 @@
+function setUpSocial() {
+
+    $('.fb').on('click', function() {
+        var url = 'https://www.facebook.com/dialog/share?app_id=809056859203594&display=popup&href=' + encodeURIComponent(document.location.href) + '&redirect_uri=' + encodeURIComponent(document.location.href) + '&ref=responsive';
+        window.open(url, '_blank');
+        return false;
+    });
+
+    $('.tw').on('click', function() {
+        var url = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(document.title + ' ' + document.location.href);
+        window.open(url, '_blank');
+        return false;
+    });
+
+    $('.email').on('click', function() {
+        var url = 'mailto:?subject=.&body=' + document.location.href;
+        window.open(url, '_blank');
+        return false;
+    });
+
+}
+
+
 function getJoke(jokes) {
     return jokes[Math.floor(Math.random()*jokes.length)];
 }
@@ -12,6 +35,8 @@ function renderHCF() {
 }
 
 $(document).ready(function() {
+
+    setUpSocial();
 
     makeJoke(jokes);
 
